45339a25ae1f0f352673a95300fd76046c5ba9f7
Maxime Hinderschiette <m.hinderschiette@acti.fr>
2024-07-10 15:29:03 +0200

    [FIX] -> Card css

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,6 +15,7 @@
 
 .start-at {
   font-size: 0.6rem;
+  width: 50%;
 }
 
 .location {
@@ -39,20 +40,18 @@
       <span>Tag 2</span>
       <span>Tag 3</span>
     </v-container>
-    <v-container width="100%" class="pt-0 pb-1 d-flex justify-space-between align-center">
-      <p class="pa-0 flash-title">Flash Art #1</p>
+    <v-container width="100%" class="pt-2 pb-1 d-flex justify-space-between align-center">
+      <v-container class="pa-0 d-flex align-center">
+        <div>
+          <v-img :width="45" aspect-ratio="1/1" class="profile-img rounded-circle" src="https://img.freepik.com/photos-gratuite/portrait-homme-riant_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1720310400&semt=ais_user"></v-img>
+        </div>
+        <div class="pl-4">
+          <v-card-text class="pa-0">Jodie SABUCO</v-card-text>
+          <p class="location"><v-icon>mdi-map-marker</v-icon> Lyon</p>
+        </div>
+
+      </v-container>
       <p class="pa-0 text-right start-at">à partir de<br> <span class="price">60€</span></p>
-    </v-container>
-
-    <v-container class="pt-0 d-flex align-center">
-      <div>
-        <v-img :width="45" aspect-ratio="1/1" class="profile-img rounded-circle" src="https://img.freepik.com/photos-gratuite/portrait-homme-riant_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1720310400&semt=ais_user"></v-img>
-      </div>
-      <div class="pl-4">
-        <v-card-text class="pa-0">Jodie SABUCO</v-card-text>
-        <p class="location"><v-icon>mdi-map-marker</v-icon> Lyon</p>
-      </div>
-
     </v-container>
 
 </v-card>
